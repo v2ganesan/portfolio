e4a1e6aaffda5533bcca0f269412dfc503fd3e16
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-12-07 01:52:11 -0800

    experience card component

---
+++ components/experienceCard.js
@@ -0,0 +1,31 @@
+'use client'
+import ReactCardFlip from "react-card-flip";
+import { useState } from "react";
+
+
+export default function ExperienceCard ( {Title, Dates, BulletPoints}) {
+    
+    const [isFlipped, flip] = useState(false)
+
+    const flipCard = () => {
+        flip(!isFlipped)
+    }
+
+    return (
+        <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped}>
+            <div className="card" onClick={flipCard}>
+                <h1>{Title}</h1>
+                <h3>{Dates}</h3>
+                
+                {BulletPoints.map((BulletPoint) => {
+                    return <h2 key={BulletPoint.id}>{BulletPoint}</h2>
+                })}
+            </div>
+
+            <div className="card" onClick={flipCard}>
+                <h1>Key Takeaways</h1>
+            </div>
+        </ReactCardFlip>
+    )
+
+}
