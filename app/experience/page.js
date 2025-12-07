733a78cec582d23769f9c8472e0766d8859bbe3c
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-12-07 01:51:47 -0800

    experiences page

--- app/experience/page.js
+++ app/experience/page.js
@@ -1,5 +1,15 @@
+import ExperienceCard from "@/components/experienceCard"
+import { USAA, Experian } from "../constants"
+
 export default function Experience (){
     return (
-        <h1> Build. Deploy. Profit. </h1>
+        <div className="experience-container">
+            <h1> Professional Experiences </h1>
+            <div className="cards-wrapper">
+                <ExperienceCard Title="Software Engineer Intern @ USAA" Dates="May 2025 - Aug 2025" BulletPoints={USAA}></ExperienceCard>
+                <ExperienceCard Title="Software Engineer Intern @ Experian" Dates="June 2023 - Sept 2023"BulletPoints={Experian}></ExperienceCard>
+            </div>
+        </div>
+
     )
 }
