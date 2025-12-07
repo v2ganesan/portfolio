2308431b49389a18d8a48cb3deb5b5cf47ac596a
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-12-07 02:19:02 -0800

    projects page / styling

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,6 +1,16 @@
-export default function Projects () {
-    return (
-        <h1> In Progress </h1>
-    )
+import ExperienceCard from "@/components/experienceCard"
+import { PlatePal, EasyCalendar, JobTrackerAgent } from "../constants"
 
+export default function Projects (){
+    return (
+        <div className="experience-container">
+            <h1> Projects </h1>
+            <div className="cards-wrapper">
+                <ExperienceCard Title="Job Application Tracker Agent" Dates="Sept 2025 - " BulletPoints={JobTrackerAgent}></ExperienceCard>
+                <ExperienceCard Title="Easy Calendar" Dates="Dec 2024 - Feb 2025"BulletPoints={EasyCalendar}></ExperienceCard>
+                <ExperienceCard Title="Plate Pal" Dates="Feb 2023 - May 2023" BulletPoints={PlatePal}></ExperienceCard>
+            </div>
+        </div>
+
+    )
 }
