686f8381bea32cd95a6e85151ef78d33a73a4adc
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-12-06 22:00:07 -0800

    Switch tags / pass in className as prop

--- components/navBar.js
+++ components/navBar.js
@@ -2,14 +2,14 @@
 use the nav tag, and embed the links for the pages i want to be accessible in the navBar 
 write a css file and put the className in the opening tag for each page in the navBar
 */
-import Link from "next/link"
-export default function NavBar() {
+//import Link from "next/link"
+export default function NavBar( {className} ) {
     return (
-        <nav>
-            <Link href="/">home</Link>
-            <Link href="/experience">experience</Link>
-            <Link href="/projects">projects</Link>
-            <Link href="/blog">blog</Link>
+        <nav className={className}>
+            <a href="/">home</a>
+            <a href="/experience">experience</a>
+            <a href="/projects">projects</a>
+            <a href="/blog">blog</a>
         </nav>
     )
 }
