15cc984a79f7bef84a142699256fe30e5f053789
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-12-06 21:52:11 -0800

    footer

---
+++ components/footer.js
@@ -0,0 +1,13 @@
+export default function Footer ( {className }) {
+    return(
+        <div className={className}>
+            <nav>
+                <a href="mailto:varun.mg554@gmail.com">email</a>
+                <a href="https://github.com/v2ganesan">github</a>
+                <a href="https://www.linkedin.com/in/v2ganesan/">linkedin</a>
+                <a href="https://open.spotify.com/user/varun.mg554?si=9aa0b89e477b4367">spotify</a>
+            </nav>
+            <p>© 2025 Varun Ganesan. All Rights Reserved.</p>
+        </div>
+    )
+}
