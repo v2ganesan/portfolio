6872831b56681d45e06a16740618ec9aa0307559
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-12-06 20:19:51 -0800

    changed global font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,21 @@
+import NavBar from '../components/navBar'
+import { Changa } from "next/font/google";
+
+/*
+font loader function
+- creates the css for changa for only english
+- saves it in the changa var 
+- now I can reference it in any opening tag as the className
+*/
+import '../app/globals.css'
+const changa = Changa({ subsets: ["latin"] });
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={changa.className}>
+        <NavBar />
         {children}
       </body>
     </html>
