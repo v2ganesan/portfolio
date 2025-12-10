ac01a56335f51ff268e8b8beffa210ab61b70ebd
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2025-12-09 17:59:32 -0800

    Corrected title of Projects page

--- app/projects/page.js
+++ app/projects/page.js
@@ -4,7 +4,7 @@ import { PlatePal, EasyCalendar, JobTrackerAgent } from "../constants"
 export default function Projects (){
     return (
         <div className="flex flex-col text-center -mt-2.5 gap-5">
-            <h1 className="text-xl"> Professional Experiences </h1>
+            <h1 className="text-xl"> Projects </h1>
             <div className="flex flex-row gap-5 flex-wrap justify-center">
                 <ExperienceCard Title="Job Application Tracker Agent" Dates="Sept 2025 - " BulletPoints={JobTrackerAgent}></ExperienceCard>
                 <ExperienceCard Title="Easy Calendar" Dates="Dec 2024 - Feb 2025"BulletPoints={EasyCalendar}></ExperienceCard>
